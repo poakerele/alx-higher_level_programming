f5e43ebbc8b1de3f728aba99dad6a18dd3fd8d3b
Peter Akerele <akerelepeter2020@gmail.com>
2022-09-20 17:34:01 +0000

    python

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def magic_calculation(a, b, c):
+    if a < b:
+        return(c)
+    elif c > b:
+        return(a + b)
+    else:
+        return(a * b - c)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
+Pri
