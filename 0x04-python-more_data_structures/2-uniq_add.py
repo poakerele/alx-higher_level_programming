7d33f43b6809eb20b652202b2739655539689f00
Peter Akerele <akerelepeter2020@gmail.com>
2022-09-28 20:26:18 +0000

    python

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,5 +1,9 @@
-q_add(my_list=[]):
-    add = 0
-    for i in set(my_list):
-        add += i
-    return add
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
