8e403f2c8307e3198a679baf73965cd24f6727eb
username <poakerele@ualr.edu>
2022-11-30 02:57:04 +0000

    SQL

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,5 @@
+i-- Removes all records with a score <= 5 in the table second_table.
+DELETE FROM `second_table`
+WHERE `score` <= 5;i-- Removes all records with a score <= 5 in the table second_table.
+DELETE FROM `second_table`
+WHERE `score` <= 5;
